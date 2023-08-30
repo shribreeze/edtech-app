87ccd5ffbe7b14b4c9e74e7f8e0377774c4ae976
Sameer Gautam <sg10682002@gmail.com>
2023-08-30 18:35:20 +0530

    home paage done

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './hero.css'
+import Heading from '../../common/heading/Heading'
 
 const Hero = () => {
   return (
@@ -7,6 +8,7 @@ const Hero = () => {
         <section className='hero'>
         <div className='container'>
           <div className='row'>
+            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
             <div className='button'>
               <button className='primary-btn'>
