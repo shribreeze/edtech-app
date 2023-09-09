b9022b447e20e7ffba6766a7764dfb67fc9a321f
Sameer Gautam <sg10682002@gmail.com>
2023-09-10 02:33:41 +0530

    added tagline

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className='container'>
           <div className='row'>
             <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <p>Empowering India's Future, One Click at a Time. Ignite Your Potential with Academia.</p>
             <div className='button'>
               <button className='primary-btn'>
                 GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
