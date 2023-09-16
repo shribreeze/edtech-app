fba44e498f6a28fc632f9431c658adf663d3876c
Sameer Gautam <sg10682002@gmail.com>
2023-09-17 01:31:11 +0530

    about section complete

--- src/components/about/AWrapper.jsx
+++ src/components/about/AWrapper.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { awrapper } from '../../dummydata'
 
 const Awrapper = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+      <section className='awrapper'>
+        <div className='container grid'>
+          {awrapper.map((val) => {
+            return (
+              <div className='box flex'>
+                <div className='img'>
+                  <img src={val.cover} alt='' />
+                </div>
+                <div className='text'>
+                  <h1>{val.data}</h1>
+                  <h3>{val.title}</h3>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </section>
+    </>
   )
 }
 
